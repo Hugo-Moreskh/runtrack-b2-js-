9d92e5a1cc6ff91e54c198e6d2a4c1d585547c17
Hugo-Moreskh <hugo.moreskh@laplateforme.io>
2024-09-06 09:33:36 +0200

    Job1

---
+++ Jour01/job1/script.js
@@ -0,0 +1,19 @@
+function myUpperCase(str) {
+
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+
+    let charCode = str.charCodeAt(i);
+    if (charCode >= 97 && charCode <= 122) {
+      result += String.fromCharCode(charCode - 32);
+    } else {
+      result += str[i];
+    }
+  }
+  console.log(result);
+  return result;
+
+}
+
+myUpperCase("aaaaaaaaaaahhhhhhhhhhhhhh");
