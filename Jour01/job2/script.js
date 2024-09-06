87fb8a20409b14724f5f7573251021d091b826f0
Hugo-Moreskh <hugo.moreskh@laplateforme.io>
2024-09-06 09:52:13 +0200

    job2

---
+++ Jour01/job2/script.js
@@ -0,0 +1,26 @@
+
+function myStrLength(str) {
+  length = 0;
+  while (str[length]) {
+    length++;
+  }
+  return length;
+}
+
+let haystack = 'La Plateforme';
+let needle = 'a';
+myStrLength(haystack);
+
+function myCountChar(haystack, needle) {
+  let length = myStrLength(haystack);
+  let count = 0;
+  for (i = 0; i < length; i++) {
+    if (haystack[i] === needle) {
+      count++;
+    }
+  }
+  return count;
+}
+let result = myCountChar(haystack, needle);
+console.log(result);
+
